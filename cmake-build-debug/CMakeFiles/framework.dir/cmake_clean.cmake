553fff4b6bd31abb8070820a79b097b0582f060c
Sebastian Reichmann <46034908+Sabbelz@users.noreply.github.com>
2020-12-09 11:00:44 +0100

    gitignore changes, because of reasons!

---
+++ cmake-build-debug/CMakeFiles/framework.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/framework.dir/external/tinyobjloader-aa07206/tiny_obj_loader.cc.obj"
+  "CMakeFiles/framework.dir/framework/source/application.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/camera_node.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/geometry_node.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/model.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/model_loader.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/node.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/point_light_node.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/scene_graph.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/shader_loader.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/texture_loader.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/utils.cpp.obj"
+  "CMakeFiles/framework.dir/framework/source/window_handler.cpp.obj"
+  "Debug/libframework.a"
+  "Debug/libframework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/solar_system.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/snoke/OneDrive/Dokumente/GitHub/120450Reichmann_GC/application/source/application_solar.cpp" "C:/Users/snoke/OneDrive/Dokumente/GitHub/120450Reichmann_GC/cmake-build-debug/CMakeFiles/solar_system.dir/application/source/application_solar.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLBINDING_STATIC_DEFINE"
+  "SYSTEM_WINDOWS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/tinyobjloader-aa07206"
+  "../external/glfw-3.1.1/include"
+  "../application/include"
+  "../framework/include"
+  "../external/glbinding-2.1.1/source/glbinding/include"
+  "external/glbinding-2.1.1/source/glbinding/include"
+  "../external/glm-0.9.6.3"
+  "../external/stb_image-2.0.6"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/snoke/OneDrive/Dokumente/GitHub/120450Reichmann_GC/cmake-build-debug/CMakeFiles/framework.dir/DependInfo.cmake"
+  "C:/Users/snoke/OneDrive/Dokumente/GitHub/120450Reichmann_GC/cmake-build-debug/external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/DependInfo.cmake"
+  "C:/Users/snoke/OneDrive/Dokumente/GitHub/120450Reichmann_GC/cmake-build-debug/external/glfw-3.1.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
